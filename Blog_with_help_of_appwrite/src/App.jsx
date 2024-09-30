7d23ad236acf4e41fa0bbb9c6a31e50e89c9a855
Anurag Jaiswal <134679771+Anurag-Jjaiswal@users.noreply.github.com>
2024-09-30 17:20:05 +0530

    Footer

--- Blog_with_help_of_appwrite/src/App.jsx
+++ Blog_with_help_of_appwrite/src/App.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./Appwrite/Auth";
+import { login, logout } from "./Store/AuthSlice";
+import { Header, Footer } from "./Components";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1 className="text-white bg-gray-700 m-4 p-4 text-3xl text-center">
-        Now Started
-      </h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 text-center">
+      <div className="w-full block">
+        <Header />
+        <main>{/* Outlet */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
